d4ff30efb55a147333e6faff1f345752188cc0a7
Jack Chen <jackchen@wildhorn.ece.pdx.edu>
2017-04-11 17:04:21 -0700

    Fixed unique tokens list

--- uwords.py
+++ uwords.py
@@ -2,12 +2,15 @@ import pandas as pd
 import numpy as np
 from nltk import word_tokenize
 Train = pd.read_csv("QuoraData/train.csv")
+Train.fillna('')
 utokens = []
 
 def uwords():
-	l = Train['question1'].tolist()
+	l = Train['question2'].tolist()
 	tokens = []
 	for i in l:
+		if type(i) != str:
+			i = " "
 		tokens = word_tokenize(i)
 		for i in tokens:
 			if i not in utokens:
