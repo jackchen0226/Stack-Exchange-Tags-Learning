82c61825cb1926674d416096aaed082e55d35418
Jack Chen <jackchen@wildhorn.ece.pdx.edu>
2017-04-27 17:02:18 -0700

    Completed data of unique word occurances, began putting features on model, restructed pickled data

--- InputQuora.py
+++ InputQuora.py
@@ -5,9 +5,11 @@ import numpy as np
 from nltk import word_tokenize
 import pickle
 import bprofile
+from 
 
 
 Train = pd.read_csv("QuoraData/train.csv")
+Train.fillna('')
 #Train = Train[:9600]
 
 def make_train_generator(train_data):
@@ -37,6 +39,7 @@ def make_train_generator(train_data):
 
 def generate_features():
 	word_to_index = {}
+	'''
 	pronouns = ['he', 'she', 'it', 'they', 'i']
 	qpkl = open('q1.pkl', 'rb')
 	q1_tokens = pickle.load(qpkl)
@@ -44,13 +47,72 @@ def generate_features():
 
 	qpkl = open('q2.pkl', 'rb')
 	q2_tokens = pickle.load(qpkl)
-	qpkl.close()
+	qpkl.close()'''
+	
+	sent = {}
+	for i in range(len(Train)):
+		if Train['question2'].iloc[i] != str:
+			sent[i] = (set(word_tokenize(Train['question1'].iloc[i])),
+						set(''))
+		else:
+			sent[i] = (set(word_tokenize(Train['question1'].iloc[i])),
+						set(word_tokenize(Train['question2'].iloc[i])))
 
-	num_features = 4
+
+	indup = defaultdict(int)
+	idup = defaultdict(int)
+	keys = set(onecount.keys())
+	for i in range(len(sent)):
+		for j in sent[i][0]:
+			if j in keys:
+				if j not in sent[i][1] and dupes.iloc[i] == 0:
+					indup[j] += 1
+				elif j not in sent[i][1] and dupes.iloc[i] == 1:
+					idup[j] += 1
+
+		for j in sent[i][1]:
+			if j in keys:
+				if j not in sent[i][0] and dupes.iloc[i] == 0:
+					indup[j] += 1
+				elif j not in sent[i][0] and dupes.iloc[i] == 1:
+					idup[j] += 1
+	
+	keys = list(onecount.keys())
+	for i in keys:
+		if onecount[i] < 10:
+			onecount.pop(i)
+
+	iflu_words = []
+	for i in idup.keys():
+		if idup[i] / onecount[i] >= 0.95:
+			iflu_words.append(i)
+	iflu_words = set(iflu_words)
+
+	influ_words = []
+	for i in indup.keys():
+		if indup[i] / onecount[i] >= 0.95:
+			influ_words.append(i)
+	influ_words = set(influ_words)
+
+	num_features = 2
 
 	def get_features(row):
 		features = np.zeros(num_features)
-		
+		# Rewrite, just need features for occurance in word, not this complex!!!
+		for i in sent[row['id']][0]:
+			if i not in sent[row['id']][1]:
+				if i in iflu_words:
+					features[0] = True
+				if i in influ_words:
+					features[1] = True
+
+		for i in sent[row['id']][1]:
+			if i not in sent[row['id']][0]:
+				if i in iflu_words:
+					features[0] = True
+				if i in influ_words:
+					features[1] = True
+
 		# Need matching words, pronoun existence as features.
 		# 0: no pronouns, 1: pronoun in q1, 2: pronoun in q2, 3: both have pronouns
 		"""for i in pronouns:
