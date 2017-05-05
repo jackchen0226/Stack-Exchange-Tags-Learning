a1fcaf18889511d8d9a01d055db59c1c038a84aa
Jack Chen <jackchen@wildhorn.ece.pdx.edu>
2017-05-04 17:53:57 -0700

    Added unique word appearance features

--- InputQuora.py
+++ InputQuora.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 from nltk import word_tokenize
 import pickle
-import bprofile
-from 
+#import bprofile
+from collections import defaultdict
 
 
 Train = pd.read_csv("QuoraData/train.csv")
@@ -19,7 +19,7 @@ def make_train_generator(train_data):
 	period = int(np.ceil(train_len / batch_size))
 
 	def generator():
-		# profiler = bprofile.BProfile('genpin.png')
+		#profiler = bprofile.BProfile('genpin.png')
 		while True:
 			i = 0
 			while i < train_len:
@@ -58,72 +58,50 @@ def generate_features():
 			sent[i] = (set(word_tokenize(Train['question1'].iloc[i])),
 						set(word_tokenize(Train['question2'].iloc[i])))
 
-
-	indup = defaultdict(int)
-	idup = defaultdict(int)
-	keys = set(onecount.keys())
-	for i in range(len(sent)):
+	# Finding unique words
+	onecount = defaultdict(int)
+	for i in sent.keys():
 		for j in sent[i][0]:
-			if j in keys:
-				if j not in sent[i][1] and dupes.iloc[i] == 0:
-					indup[j] += 1
-				elif j not in sent[i][1] and dupes.iloc[i] == 1:
-					idup[j] += 1
-
+			if j not in sent[i][1]:
+				onecount[j] += 1
+				
 		for j in sent[i][1]:
-			if j in keys:
-				if j not in sent[i][0] and dupes.iloc[i] == 0:
-					indup[j] += 1
-				elif j not in sent[i][0] and dupes.iloc[i] == 1:
-					idup[j] += 1
-	
-	keys = list(onecount.keys())
-	for i in keys:
+			if j not in sent[i][0]:
+				onecount[j] += 1
+	bothcount = defaultdict(int)
+	for i in sent.keys():
+		for j in sent[i][0]:
+			if j in sent[i][1]:
+				bothcount[j] += 1
+
+	# Trimming words that appear less than 10 times
+	okeys = set(onecount.keys())
+	for i in okeys:
 		if onecount[i] < 10:
 			onecount.pop(i)
 
-	iflu_words = []
-	for i in idup.keys():
-		if idup[i] / onecount[i] >= 0.95:
-			iflu_words.append(i)
-	iflu_words = set(iflu_words)
+	bkeys = set(bothcount.keys())
+	for i in bkeys:
+		if bothcount[i] < 10:
+			bothcount.pop(i)
 
-	influ_words = []
-	for i in indup.keys():
-		if indup[i] / onecount[i] >= 0.95:
-			influ_words.append(i)
-	influ_words = set(influ_words)
 
 	num_features = 2
 
 	def get_features(row):
 		features = np.zeros(num_features)
 		# Rewrite, just need features for occurance in word, not this complex!!!
-		for i in sent[row['id']][0]:
-			if i not in sent[row['id']][1]:
-				if i in iflu_words:
-					features[0] = True
-				if i in influ_words:
-					features[1] = True
-
-		for i in sent[row['id']][1]:
-			if i not in sent[row['id']][0]:
-				if i in iflu_words:
-					features[0] = True
-				if i in influ_words:
-					features[1] = True
-
-		# Need matching words, pronoun existence as features.
-		# 0: no pronouns, 1: pronoun in q1, 2: pronoun in q2, 3: both have pronouns
-		"""for i in pronouns:
-			if i not in q1_tokens[row['id']] and i not in q2_tokens[row['id']]:
+		# 0 : A unique word is in one question but not the other
+		# 1 : A unique word is in both questions
+		l1 = set(word_tokenize(row['question1']))
+		l2 = str(row['question2'])
+		for i in l1:
+			if i in okeys and i not in l2:
 				features[0] = True
-			if i in q1_tokens[row['id']] and i not in q2_tokens[row['id']]:
+			if i in bkeys and i in l2:
 				features[1] = True
-			if i not in q2_tokens[row['id']] and i in q2_tokens[row['id']]:
-				features[2] = True
-			if i in q1_tokens[row['id']] and i in q2_tokens[row['id']]:
-				features[3] = True"""
+
+
 		return features
 	return num_features, get_features
 
@@ -149,7 +127,7 @@ def main():
 	split = len(Train) * 9 // 10
 	gen, gen_len = make_train_generator(Train[:split])
 	val_gen, val_len = make_train_generator(Train[split:])
-	k.fit_generator(generator=gen, steps_per_epoch=gen_len, epochs=1,
+	k.fit_generator(generator=gen, steps_per_epoch=gen_len, epochs=10,
 			validation_data=val_gen, validation_steps=val_len)
 	#k.evaluate()
 
